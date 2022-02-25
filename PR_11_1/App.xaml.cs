c18ef05f6321a37db52fd77b2d11c2c90414d91f
civilmob1234 <raiden123926@gmail.com>
2022-02-25 15:28:47 +0300

    Добавьте файлы проекта.

---
+++ PR_11_1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PR_11_1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new VerticalPage();
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PR_11_1/VerticalPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PR_11_1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VerticalPage : ContentPage
+	{
+		public VerticalPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
